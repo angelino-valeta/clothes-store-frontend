2f07ee6e55ca580153f4e16352d53ad0e3224aa1
Angelino Valeta <angelinovaleta@gmail.com>
2021-07-08 20:17:16 +0100

    add page - cart

---
+++ src/views/Cart/Cart.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Carrinho de compras
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Cart",
+  components: {
+   
+  },
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+
+<style>
+</style>
