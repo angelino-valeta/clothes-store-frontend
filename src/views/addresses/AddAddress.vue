1da357a0edea3c10ab52b0c13014ac796624469d
Angelino Valeta <angelinovaleta@gmail.com>
2021-07-08 20:20:00 +0100

    add page - addresses

---
+++ src/views/addresses/AddAddress.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Add Address
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Address",
+  components: {
+   
+  },
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+
+<style>
+</style>
