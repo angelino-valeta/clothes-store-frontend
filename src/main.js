946d9cefa6791e3c04aafa21368230ad0e7ceae5
Angelino Valeta <angelinovaleta@gmail.com>
2021-07-08 08:37:22 +0100

    setup vue

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router"
+import store from "./store"
+
+import { sync } from 'vuex-router-sync'
+const unsync = sync(store, router)
+
+import "@/assets/styles/tailwind.css";
+
+Vue.config.productionTip = false;
+unsync();
+
+import "./assets/styles/tailwind.css";
+import "./assets/styles/fonts.css";
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
