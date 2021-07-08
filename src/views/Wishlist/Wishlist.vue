778a70742310b5c9a8e24bdd5c452e37263b8514
Angelino Valeta <angelinovaleta@gmail.com>
2021-07-08 20:15:55 +0100

    add page - wishlist

---
+++ src/views/Wishlist/Wishlist.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    Roupas favoritas
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "WishList",
+  components: {
+   
+  },
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+
+<style>
+</style>
