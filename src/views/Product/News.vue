76baf5fbf67feb042925c694d9bb630288c3f93c
Angelino-Valeta <angelinovaleta@gmail.com>
2021-07-16 19:21:42 +0100

    add view for news products

---
+++ src/views/Product/News.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="w-full xl:max-w-screen-xl mx-auto flex items-center justify-between">
+      <h2 class="text-xl font-semibold text-gray-500">Novidades</h2>
+    </div>
+    <NewsProducts />
+  </div>
+</template>
+
+<script>
+import NewsProducts from "@/components/Products/NewsProducts.vue";
+export default {
+  name: "News",
+  components: {
+    NewsProducts
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style>
+</style>
